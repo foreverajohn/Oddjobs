53e3d816ab00d0d0fbe842bf66213547712d725a
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-03-20 07:58:15 -0500

    added profile page and changes to the design

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
+
 $(function() {
   $(".needCarousel, .offerCarousel").flickity({
     // options
-    cellAlign: 'left',
+    cellAlign: 'right',
     contain: true,
     freeScroll: true,
     prevNextButtons: false,
-    pageDots: false,
     autoPlay: true
   });
+
+
+  $("#toggle, #toggleMd").click(function(){
+    var toggle;
+    var right = $("#smrect, #smrectMd").css('right')
+    if (right == '115px'){
+      $("#smrect, #smrectMd").css({right: "60px"});
+      $("#need, #needMd").attr("src","img/Need_alt.svg")
+      $("#offer, #offerMd").attr("src","img/Offer_alt.svg")
+    } else {
+      $("#smrect, #smrectMd").css({right: "115px"});
+      $("#need, #needMd").attr("src","img/Need.svg")
+      $("#offer, #offerMd").attr("src","img/Offer.svg")
+    };
+  });
+
 });
+
+
